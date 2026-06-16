aa7eb16c3c05106f59b051682d88b79c1afd35c4
Raima Saha <raimasaha@CBKs-MacBook-Pro.local>
2026-06-16 14:25:38 -0400

    made minor edits to carousel

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-// theme switch functionality
-
-const themeSwitch = document.getElementById('theme-switch');
-const themeText = document.getElementById('theme-text'); // Targets the text element
+const themeSwitch = document.getElementById("theme-switch");
+const themeText = document.getElementById("theme-text"); // Targets the text element
 
 // check local storage on page load
-const isDarkMode = localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+const isDarkMode =
+  localStorage.getItem("theme") === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches);
 
 // synchronize HTML class, slider handle, and text label on initial load
 if (isDarkMode) {
-  document.documentElement.classList.add('dark');
+  document.documentElement.classList.add("dark");
   themeSwitch.checked = true;
-  themeText.textContent = 'Dark Mode';
+  themeText.textContent = "Dark Mode";
 } else {
-  document.documentElement.classList.remove('dark');
+  document.documentElement.classList.remove("dark");
   themeSwitch.checked = false;
-  themeText.textContent = 'Light Mode';
+  themeText.textContent = "Light Mode";
 }
 
 // listen for changes on the sliding toggle check event
-themeSwitch.addEventListener('change', () => {
+themeSwitch.addEventListener("change", () => {
   if (themeSwitch.checked) {
-    document.documentElement.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
-    themeText.textContent = 'Dark Mode'; // Changes text to Dark Mode
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    themeText.textContent = "Dark Mode"; // Changes text to Dark Mode
   } else {
-    document.documentElement.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
-    themeText.textContent = 'Light Mode'; // Changes text to Light Mode
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    themeText.textContent = "Light Mode"; // Changes text to Light Mode
   }
 });
 
